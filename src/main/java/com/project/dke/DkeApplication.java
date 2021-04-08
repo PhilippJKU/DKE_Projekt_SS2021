7c546746abb8fe3e3476b130c52e8ec913ca5464
PhilippJKU <p.engelbrechtsmueller@gmx.at>
2021-04-08 15:25:29 +0200

    Initial commit

---
+++ src/main/java/com/project/dke/DkeApplication.java
@@ -0,0 +1,13 @@
+package com.project.dke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/dke/DkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
